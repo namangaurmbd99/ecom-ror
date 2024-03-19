1ed6ce9a0c747f32ea983f1feda49824c21dbbd3
Naman Gaur <namang@mindfiresolutions.com>
2024-03-19 16:09:34 +0530

    refactored code

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,50 +1,28 @@
 class CheckoutController < ApplicationController
+  before_action :set_order, only: [:confirm_order, :place_order, :order_confirmation]
+
   def confirm_order
-    puts "Confirming order11111111" # Debugging output
     @cart_products = current_user.cart.cart_products.includes(:product)
     @order = current_user.orders.build
   end
 
   def place_order
-    puts "Placing order111111" # Debugging output
     @order = current_user.orders.build(order_params)
-    puts "Order before saving: #{@order}" # Debugging output
-    puts "Order errors: #{@order.errors.full_messages}" if @order.invalid? # Debugging output
     @order.total_amount = 0
     @cart_products = current_user.cart.cart_products.includes(:product)
     
-    if @cart_products.empty?
-      flash[:alert] = 'Your cart is empty. Please add items to your cart before placing an order.'
-      redirect_to cart_path
-      return
-    end
+    return redirect_empty_cart if @cart_products.empty?
     
     if @order.save
-      @cart_products.each do |cart_product|
-        order_product = @order.order_products.build(
-          product: cart_product.product,
-          quantity: cart_product.quantity,
-          amount: cart_product.product.price * cart_product.quantity
-        )
-        puts "Created order product: #{order_product.inspect}" # Debugging output
-        @order.total_amount += order_product.amount
-      end
-      
-      # Assign payment_method to the order
-      @order.update(payment_method: order_params[:payment_method])
-      
-      @order.save # Save the order again to update the total_amount
-      current_user.cart.cart_products.destroy_all
-      redirect_to order_confirmation_path(order_id: @order.id), notice: 'Order placed successfully!'
+      update_order_products
+      redirect_after_order_success
     else
       flash[:alert] = 'Failed to place order.'
       render :confirm_order
     end
   end
   
-  
   def order_confirmation
-    @order = current_user.orders.last
     if @order.nil?
       flash[:alert] = "No orders found."
       redirect_to root_path
@@ -55,9 +33,34 @@ class CheckoutController < ApplicationController
   
   private
   
+  def set_order
+    @order = current_user.orders.last
+  end
+  
   def order_params
     params.require(:order).permit(:address_id, :payment_method)
   end
   
-   
+  def redirect_empty_cart
+    flash[:alert] = 'Your cart is empty. Please add items to your cart before placing an order.'
+    redirect_to cart_path
+  end
+  
+  def update_order_products
+    @cart_products.each do |cart_product|
+      order_product = @order.order_products.build(
+        product: cart_product.product,
+        quantity: cart_product.quantity,
+        amount: cart_product.product.price * cart_product.quantity
+      )
+      @order.total_amount += order_product.amount
+    end
+    @order.update(payment_method: order_params[:payment_method])
+    @order.save
+    current_user.cart.cart_products.destroy_all
+  end
+  
+  def redirect_after_order_success
+    redirect_to order_confirmation_path(order_id: @order.id), notice: 'Order placed successfully!'
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,56 +1,54 @@
 class OrdersController < ApplicationController
-    before_action :set_order, only: [:show, :edit, :update, :destroy]
-  
-    def index
-        authorize Order
-        @orders = current_user.orders.includes(:order_products)
-    end
-  
-    def show
-    end
-  
-    def new
-        authorize Order
-        @order = current_user.orders.build
-    end
-  
-    def create
-        authorize Order
-        @order = current_user.orders.build(order_params)
-        if @order.save
-            redirect_to @order, notice: 'Order was successfully created.'
-        else
-            render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-        authorize Order
-      if @order.update(order_params)
-        redirect_to @order, notice: 'Order was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-        authorize Order
-      @order.destroy
-      redirect_to orders_url, notice: 'Order was successfully destroyed.'
-    end
-  
-    private
-  
-    def set_order
-      @order = current_user.orders.find(params[:id])
-    end
-  
-    private
-    def order_params
-      params.require(:order).permit(:user_id, :total_amount) # Add other permitted attributes as needed
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  def index
+    authorize Order
+    @orders = current_user.orders.includes(:order_products)
+  end
+
+  def show
+  end
+
+  def new
+    authorize Order
+    @order = current_user.orders.build
+  end
+
+  def create
+    authorize Order
+    @order = current_user.orders.build(order_params)
+    save_order('Order was successfully created.', :new)
+  end
+
+  def edit
+  end
+
+  def update
+    authorize Order
+    save_order('Order was successfully updated.', :edit)
+  end
+
+  def destroy
+    authorize Order
+    @order.destroy
+    redirect_to orders_url, notice: 'Order was successfully destroyed.'
+  end
+
+  private
+
+  def set_order
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:user_id, :total_amount)
+  end
+
+  def save_order(success_message, render_action)
+    if @order.update(order_params)
+      redirect_to @order, notice: success_message
+    else
+      render render_action
     end
   end
-  
+end
